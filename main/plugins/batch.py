1e94f92ae5d7839971a3b349ac59b9d3a1c2e9ea
Shankar <120117175+Saanusalvi1@users.noreply.github.com>
2024-05-27 13:17:28 +0530

    Update batch.py

--- main/plugins/batch.py
+++ main/plugins/batch.py
@@ -90,21 +90,21 @@ async def cancel(event):
     
 async def run_batch(userbot, client, sender, countdown, link):
     for i in range(len(ids)):
-        timer = 6
+        timer = 60
         if i < 250:
-            timer = 2
-        elif i < 1000 and i > 100:
-            timer = 3
-        elif i < 10000 and i > 1000:
-            timer = 4
-        elif i < 50000 and i > 10000:
-            timer = 5
-        elif i < 100000 and i > 50000:
-            timer = 6
-        elif i < 200000 and i > 100000:
-            timer = 8
-        elif i < 1000000: 
             timer = 10
+        elif i < 1000 and i > 100:
+            timer = 12
+        elif i < 10000 and i > 1000:
+            timer = 14
+        elif i < 50000 and i > 10000:
+            timer = 16
+        elif i < 100000 and i > 50000:
+            timer = 18
+        elif i < 200000 and i > 100000:
+            timer = 20
+        elif i < 1000000: 
+            timer = 22
         
         if 't.me/c/' not in link:
             timer = 1 if i < 500 else 2
